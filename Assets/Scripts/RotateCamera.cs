e58a9bdc7ffe6e5fcc27bfb9d14ff511525a7b8a
BUCKERINO <tychovandewetering@gmail.com>
2024-04-17 10:05:44 +0200

    cam can rotate around the Y axis

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField] private Transform focusPoint;
+    [SerializeField] private Transform camPos;
+
+    [SerializeField] private float sensX;
+    [SerializeField] private float sensY;
+
+    private float xRotation;
+    private float yRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            xRotation -= Time.deltaTime * sensX * 10;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            xRotation += Time.deltaTime * sensX * 10;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            yRotation -= Time.deltaTime * sensY * 10;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            yRotation += Time.deltaTime * sensY * 10;
+        }
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, xRotation, transform.rotation.eulerAngles.z);
+    }
+}
