2ee89c397c624c90cdea90879c0b543995e6902f
ThijsBosma <bosmathijs005@gmail.com>
2024-04-19 14:17:43 +0200

    Moving platforms finished

--- Assets/Scripts/Interactables/AnimationInteractable.cs
+++ Assets/Scripts/Interactables/AnimationInteractable.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationInteractable : MonoBehaviour, IPlayerData
+public class AnimationInteractable : MonoBehaviour, IInteractable
 {
     [SerializeField] private Animator _InteractAnimation;
     [SerializeField] private GameObject _DuckPrefab;
     [SerializeField] private Transform _DuckSpawnPoint;
     
-    public void CollectDuck(PlayerData playerData)
+    public void Interact()
     {
         _InteractAnimation.SetBool("IsPressed", true);
     }
