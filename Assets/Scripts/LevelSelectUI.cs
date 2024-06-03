508bd24e2674b13312ae67d63f7f2dd7072834c4
ThijsBosma <bosmathijs005@gmail.com>
2024-06-03 14:08:47 +0200

    Level selection works

---
+++ Assets/Scripts/LevelSelectUI.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectUI : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _Levels;
+    private int _currentIndex;
+
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ShowNextLevel()
+    {
+        _Levels[_currentIndex].SetActive(false);
+        _currentIndex += 1;
+        IsIndexOutOfBounds();
+        _Levels[_currentIndex].SetActive(true);
+    }
+
+    public void ShowPreviousLevel()
+    {
+        _Levels[_currentIndex].SetActive(false);
+        _currentIndex -= 1;
+        IsIndexOutOfBounds();
+        _Levels[_currentIndex].SetActive(true);
+    }
+
+    public void IsIndexOutOfBounds()
+    {
+        if(_currentIndex >= _Levels.Length)
+        {
+            _currentIndex = 0;
+        }
+        if(_currentIndex < 0)
+        {
+            _currentIndex = _Levels.Length - 1;
+        }
+    }
+}
