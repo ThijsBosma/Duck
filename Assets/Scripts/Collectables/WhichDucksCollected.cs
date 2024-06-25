4629a249766131d92d8916205e92e4260b02c355
ThijsBosma <bosmathijs005@gmail.com>
2024-06-25 11:57:20 +0200

    Duck ids show correctly again

--- Assets/Scripts/Collectables/WhichDucksCollected.cs
+++ Assets/Scripts/Collectables/WhichDucksCollected.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class WhichDucksCollected : MonoBehaviour
 {
-    [SerializeField, Tooltip("The ID that corresponds to the level")] private int _ImageID;
+    [SerializeField, Tooltip("The ID that corresponds to the duck id in the level")] private int _ImageID;
 
     private GameObject _ImageObject;
+    private bool _isActivated;
 
     private void Start()
     {
@@ -15,13 +16,24 @@ public class WhichDucksCollected : MonoBehaviour
         if (PlayerData._Instance._DuckIDs.Contains(_ImageID))
         {
             _ImageObject.SetActive(true);
+            _isActivated = true;
             Debug.Log("ID Found");
         }
         else
         {
             _ImageObject.SetActive(false);
+            _isActivated = false;
             Debug.Log("No ID found");
         }
     }
+
+    //private void Update()
+    //{
+    //    if (PlayerData._Instance._DuckIDs.Contains(_ImageID) && _isActivated == false)
+    //    {
+    //        _ImageObject.SetActive(true);
+    //        Debug.Log("ID Found");
+    //    }
+    //}
 
 }
